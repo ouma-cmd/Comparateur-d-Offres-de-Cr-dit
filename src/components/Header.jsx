216723e077802ba310cfb347dfa8c6b9fc0920bf
oueldelalia <oueldelalia@email.com>
2026-04-21 17:35:28 +0100

    header

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <header className="headerstyle">
+      <h1 className="titlestyle">CREDIT BANK</h1>
+    </header>
+  );
+};
+
+export default Header;
