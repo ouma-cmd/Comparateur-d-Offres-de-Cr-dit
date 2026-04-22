ef2b38dcc7d16f0f71dc58b4989dedd73fc825bf
oueldelalia <oueldelalia@email.com>
2026-04-22 12:52:01 +0100

    cree function credit card

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,18 @@
-
-import Header from './components/header'
+import CreditCard from "./components/CreditCard";
+import offers from "./data/data";
+import Header from "./components/Header";
 
 function App() {
-
   return (
-    <>
-      <Header ></Header>
-
-
-    </>
-  )
+    <div>
+      <Header></Header>
+      <div className="container">
+        {offers.map((offer) => (
+          <CreditCard key={offer.id} offer={offer} />
+        ))}
+      </div>
+    </div>
+  );
 }
 
-export default App ;
+export default App;
